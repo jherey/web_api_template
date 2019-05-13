bad6d1a94545bfbb394644a6e3bb9bafb2cc5006
Olufayo Jeremiah <olufayojeremiah@gmail.com>
2019-05-13 13:39:46 +0100

    add todo schema

--- src/data/models/TodoSchema.js
+++ src/data/models/TodoSchema.js
@@ -2,13 +2,14 @@ import { SchemaTypes } from 'mongoose';
 import BaseSchema from './BaseSchema';
 
 const todoSchema = new BaseSchema({
-  todo: {
+  title: {
     type: SchemaTypes.String,
     required: true,
     index: true,
   },
   priority: {
     type: Number,
+    required: true,
   },
   isCompleted: {
     type: Boolean,
