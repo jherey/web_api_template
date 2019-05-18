9a928dd5193e0050493a829fa7d4fc83af100b71
Olufayo Jeremiah <olufayojeremiah@gmail.com>
2019-05-18 21:29:47 +0100

    add required env variables

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// required environment variables
+[
+  'NODE_ENV', 'PORT', 'API_PREFIX', 'AGENDA_COLLECTION',
+].forEach((name) => {
+  if (!process.env[name]) {
+    throw new Error(`Environment variable ${name} is missing`);
+  }
+});
+
 export default {
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
-  databaseURL: process.env.DATABASE_URL,
+  db: {
+    test: process.env.TEST_DATABASE_URL,
+    dev: process.env.DATABASE_URL,
+  },
   logs: {
     label: process.env.LOG_LABEL,
     level: process.env.LOG_LEVEL,
@@ -12,5 +25,8 @@ export default {
   },
   api: {
     prefix: process.env.API_PREFIX,
+  },
+  agenda: {
+    dbCollection: process.env.AGENDA_COLLECTION,
   },
 };
